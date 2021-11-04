5b12d596673df3bfd9034c131ecd0b410b9332cf
WikiWikiWasp <jcflinn14@gmail.com>
2021-11-04 11:57:18 -0500

    move data into separate file, add butthole meme, exclude f.fam for reaction add, add unknown command msg

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from datetime import datetime as dt
 from discord.ext import commands
 from config.settings import DISCORD_TOKEN
+from structs.responses import *
 from re import search
 
 client = discord.Client
@@ -32,19 +33,14 @@ famDict = {
     ]
 }
 
-famList = [
-    "WikiWikiWasp",
-    "Wirt.zirp",
-    "bossanova",
-    "Snail",
-    "The Mongoose",
-    "shaggyzero",
-    "ToeUp"
-]
-
 @bot.event
 async def on_ready():
-    print("suh")
+    print("suh fam")
+
+@bot.event
+async def on_command_error(ctx, error):
+    print(f'{ctx.author} tried to use {ctx.message.content}')
+    await ctx.send(random.choice(err_msg))
     
 @bot.event
 async def on_message(msg):
@@ -59,10 +55,8 @@ async def on_message(msg):
     if msg.author == bot.user:
         return
     
-    #if msg.content.startswith("f.") and msg.content is not in bot.commands.
-    
     # fam react
-    if 'fam' in msg.content:
+    if 'fam' in msg.content and not msg.content.startswith('f.'):
         emoji = discord.utils.get(msg.guild.emojis, name='FAM')
         if emoji:
             await msg.add_reaction(emoji)
@@ -73,9 +67,16 @@ async def on_message(msg):
         or msg.content.endswith(' hava') \
         or msg.content == 'hava':
         await msg.channel.send('hava nice day fam lmao gottem')
+    
+    # butthole
+    if search('looking for', msg.content) \
+        or search('where is', msg.content) \
+        or search('where are', msg.content):
+        await msg.channel.send('https://c.tenor.com/hmwml17QnQ8AAAAC/tom-cardy-butthole.gif')
 
 @bot.command()
 async def help(ctx):
+    print(f'{ctx.author} used f.help')
     embed = discord.Embed(
         title='Bot Commands',
         description='You looking for help, fam? Have you checked your butthole?',
@@ -114,44 +115,15 @@ async def fam(ctx):
     """
     TODO: random 'fam' meme
     """
-    memes = [
-        'ur just fucking dying',
-        'ur not fam',
-        'u look like a bird',
-        'look at your hair\nnow look at mine',
-        '**F A M**',
-        'please touch it',
-        'did it hurt?\nwhen you fell from someone\'s butthole into toilet water you piece of shit',
-        'w o w',
-        'oh hi, mark',
-        'ur moves are weak',
-        'please...my wets',
-        discord.utils.get(ctx.guild.emojis, name='FAM'),
-        discord.utils.get(ctx.guild.emojis, name='sideeye'),
-        discord.utils.get(ctx.guild.emojis, name='thor')
-    ]
-
-    meme = random.choice(memes)
-    await ctx.send(meme)
+    print(f'{ctx.author} used f.fam')
+    await ctx.send(random.choice(memes))
 
 @bot.command()
 async def amifam(ctx):
     """
     TODO: fam 'rank' based on how many times they've said 'fam' or used :FAM: on the server
     """
-    # await ctx.send('idk yet, still feelin yall out')
-    nos = [
-        'lol no',
-        'u wish',
-        'ur moves are too weak to be fam',
-        'not yet',
-        'ur fam...ously stupid lmao gottem',
-        'u look like a bird',
-        'ur just a superdick',
-        'go play away and do something in ur life',
-        'ur just a real stupid'
-    ]
-
+    print(f'{ctx.author} used f.amifam')
     if any(ctx.author.name in js for js in famDict['jsquad']):
         await ctx.send('Fam AND JSquad. Jam, if you will.')
     elif any(ctx.author.name in fam for fam in famDict['isfam']):
@@ -163,12 +135,15 @@ async def amifam(ctx):
 
 @bot.command()
 async def time(ctx):
+    print(f'{ctx.author} used f.time')
     fam_channels = []
     v_channels = ctx.guild.voice_channels
+
     for v_chan in v_channels:
         for user in v_chan.members:
             if any(user.name in fam for fam in famDict['isfam']):
                 fam_channels.append(v_chan.name)
+
     if dt.now().hour > 21 or dt.now().hour < 3:
         await ctx.send('it\'s that time of night, fam')
         if len(fam_channels) == 1:
@@ -184,6 +159,7 @@ async def time(ctx):
 
 @bot.command()
 async def meme(ctx, *args):
+    print(f'{ctx.author} used f.meme')
     meme_arg = ' '.join(args)
     if 'wets' in meme_arg:
         await ctx.send('https://c.tenor.com/AxZlzVC4rrMAAAAM/parmesan-parmiggiano.gif')
@@ -193,5 +169,8 @@ async def meme(ctx, *args):
         await ctx.send('HAVA NICE DAY LMAO GOTTEM FAM')
     elif 'moves' in meme_arg or 'weak' in meme_arg:
         await ctx.send('https://c.tenor.com/ULv-OVA89isAAAAM/moves-are-weak-upper-hook.gif')
+    elif 'butthole' in meme_arg:
+        await ctx.send('_ski-bap ba-dap **butthole**_')
+        await ctx.send('https://c.tenor.com/fYkgtSeoiokAAAAC/tomcardy-have-you-checked-your-butthole.gif')
 
 bot.run(DISCORD_TOKEN)
---
+++ structs/responses.py
@@ -0,0 +1,43 @@
+memes = [
+    'ur just fucking dying',
+    'ur not fam',
+    'u look like a bird',
+    'look at your hair\nnow look at mine',
+    '**F A M**',
+    'please touch it',
+    'did it hurt?\nwhen you fell from someone\'s butthole into toilet water you piece of shit',
+    'w o w',
+    'oh hi, mark',
+    'ur moves are weak',
+    'please...my wets'
+    '<:FAM:848761741102153739>',
+    '<a:sideeye:765608935943176282>',
+    '<:thor:669591170095120424>'
+]
+
+nos = [
+    'lol no',
+    'u wish',
+    'ur moves are too weak to be fam',
+    'not yet',
+    'ur fam...ously stupid lmao gottem',
+    'u look like a bird',
+    'ur just a superdick',
+    'go play away and do something in ur life',
+    'ur just a real stupid'
+]
+
+err_msg = [
+    'bruh, idk what u want me to do with that',
+    'wut',
+    'wtf?',
+    'okay...',
+    'umm...',
+    'u ok, fam?',
+    'y tho',
+    'nah',
+    'u just like making up stuff, huh?',
+    'we just making up stuff now?',
+    'fam. pls.',
+    '<:wutthe:672669825960509450>'
+]
