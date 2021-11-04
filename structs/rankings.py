41f4c19f7d24cd4741ae7f3e5a89bb21c1135f02
WikiWikiWasp <jcflinn14@gmail.com>
2021-11-04 17:25:10 -0500

    add fam users and rank json files

---
+++ structs/rankings.py
@@ -0,0 +1,12 @@
+rank_title = {
+    1: 'Weak',
+    2: 'Step Fam',
+    3: 'FAM',
+    4: 'True Fam',
+    5: 'Elder Fam',
+    6: 'Royal Fam',
+    7: 'Super Fam',
+    8: 'Ultra Fam',
+    9: 'God Fam',
+    10: 'OnlyFams'
+}
