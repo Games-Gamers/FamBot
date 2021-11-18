429a7aac6cc285d6669646e2316c478c79ee5c74
WikiWikiWasp <jcflinn14@gmail.com>
2021-11-18 13:15:50 -0600

    #12 - add ranking helper func

--- structs/rankings.py
+++ structs/rankings.py
@@ -1,3 +1,5 @@
+import json
+
 rank_title = {
     1: 'Weak',
     2: 'Step Fam',
@@ -10,3 +12,19 @@ rank_title = {
     9: 'God Fam',
     10: 'OnlyFams'
 }
+
+def fam_by_rank(fam_rank):
+    if isinstance(fam_rank, int):
+        fam_rank = str(fam_rank)
+
+    fam = []
+    with open('structs/users.json', 'r') as f:
+        users = json.load(f)
+
+    for user_id in users:
+        if any(fam_rank in rank for rank in users[user_id]["rank"]):
+            fam.append(users[user_id]["name"])
+    
+    fam_str = "\n".join(fam)
+    
+    return fam_str
