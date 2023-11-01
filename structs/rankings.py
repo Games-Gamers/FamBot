3d9617b9e472dfcff2b878040bd288f3191629ae
WikiWikiWasp <jcflinn14@gmail.com>
2023-11-01 15:01:27 -0500

    removed references to fam rankings features to prevent failed builds from unstable dependencies

--- structs/rankings.py
+++ structs/rankings.py
@@ -1,18 +1,5 @@
 import json
 
-rank_title = {
-    1: 'Weak',
-    2: 'Step Fam',
-    3: 'FAM',
-    4: 'True Fam',
-    5: 'Elder Fam',
-    6: 'Royal Fam',
-    7: 'Super Fam',
-    8: 'Ultra Fam',
-    9: 'God Fam',
-    10: 'OnlyFams'
-}
-
 famDict = {
     "isfam": [
         "WikiWikiWasp",
@@ -29,17 +16,3 @@ famDict = {
         "shaggyzero",
     ]
 }
-
-def fam_by_rank(fam_rank, users):
-
-    fam = []
-
-    for user_id in users:
-        if users[user_id]["rank"] == fam_rank:
-            fam.append(users[user_id]["name"])
-
-    if fam:
-        fam_str = "\n".join(fam)
-        return fam_str
-    else:
-        return "none"
