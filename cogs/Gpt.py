ecdc73d75d62fccac45e19d9ed79c54641abc94e
mulchbutler <mulchbutler@att.net>
2024-06-06 15:03:42 -0500

    Update Gpt.py

--- cogs/Gpt.py
+++ cogs/Gpt.py
@@ -4,7 +4,7 @@ from config.settings import GPT_TOKEN
 import re
 
 bot_name = "butlerbot"
-model = "gpt-4-1106-preview"
+model = "gpt-4-turbo"
 target_channel = 1048743997432143902
 memory_limit = 40
 prompt = f"""
